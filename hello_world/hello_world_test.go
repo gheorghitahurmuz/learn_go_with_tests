f3e8008c3d5d5745b938a03a3f6b09a51284373c
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-02 17:09:47 +0300

    Add default value for name

--- hello_world/hello_world_test.go
+++ hello_world/hello_world_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Gheorghita")
-	want := "Hello, Gheorghita!"
+	// these are subtests
+	// they are useful to group tests aroung a "thing" and describing different scenarios
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Gheorghita")
+		want := "Hello, Gheorghita!"
 
-	if got != want {
-		t.Errorf("got: %q,  want: %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got: %q,  want: %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hell, World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got: %q,  want: %q", got, want)
+		}
+	})
 }
