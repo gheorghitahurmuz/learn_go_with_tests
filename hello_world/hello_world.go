b15011b39704effe1426dc436e6db54804c3bcf3
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-02 17:25:52 +0300

    Extract out the greeting prefix functionality

--- hello_world/hello_world.go
+++ hello_world/hello_world.go
@@ -2,30 +2,37 @@ package main
 
 import "fmt"
 
-const romanian = "Romanian"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const romanianHelloPrefix = "Salut, "
-const frenchHelloPrefix = "Bonjour, "
-const defaultName = "World"
-const suffix = "!"
+const (
+	romanian = "Romanian"
+	french = "French"
 
-func Hello(name string, language string) string {
+	englishHelloPrefix = "Hello, "
+	romanianHelloPrefix = "Salut, "
+	frenchHelloPrefix = "Bonjour, "
+
+	defaultName = "World"
+
+	suffix = "!"
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = defaultName
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + suffix
+}
 
-	
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case romanian:
 		prefix = romanianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name + suffix
+	return
 }
 
 func main() {
