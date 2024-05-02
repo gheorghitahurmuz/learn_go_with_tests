b7f9e3989cf8fde6bffb36d2a641c0cd5581c6a1
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-02 17:14:19 +0300

    Refactor test suit: extract duplicate code

--- hello_world/hello_world_test.go
+++ hello_world/hello_world_test.go
@@ -8,18 +8,21 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Gheorghita")
 		want := "Hello, Gheorghita!"
-
-		if got != want {
-			t.Errorf("got: %q,  want: %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hell, World!' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got: %q,  want: %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// this tell to test suite that this method is a helper
+	// it help to track down problems easier (show the correct line from test)
+	t.Helper()
+	if got != want {
+		t.Errorf("got: %q,  want: %q", got, want)
+	}
 }
