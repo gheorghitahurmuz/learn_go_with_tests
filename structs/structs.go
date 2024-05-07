0402860ccb4787a5568e014b0515bb9b71d915d6
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-07 11:15:19 +0300

    Use structs and interfaces

---
+++ structs/structs.go
@@ -0,0 +1,35 @@
+package structs
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// a method is a function with a receiver
+// a method declaration binds an identifier, the method name, to a method, and associates the method with the receiver's base type
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
