3362abbc2e9a5460a108198ba3d7b5b858f2b334
Gheorghita Hurmuz <hurmuz.gheorghita@pm.me>
2024-05-07 11:25:18 +0300

    Use table driven tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -31,3 +31,26 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 }
+
+func TestTableDreivenArea(t *testing.T) {
+	// anonymous struct
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		// Kent Beck: The test speaks to us more clearly, as if it were an assertion of truth, not a sequence of operations
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
